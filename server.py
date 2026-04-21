0bf170ca45bba862bd0d828fb3984e6579203e0b
mr_lai-13615002926 <1106719671@qq.com>
2026-04-21 21:16:19 +0800

    优化了html代码，图片以及下载链接json配置化，整理了仓库结构

--- server.py
+++ server.py
@@ -3,23 +3,28 @@ import socketserver
 import webbrowser
 import os
 
-PORT = 8000
+try :
+    PORT = 8000
 
-# 设置工作目录为脚本所在目录
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # 设置工作目录为脚本所在目录
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-Handler = http.server.SimpleHTTPRequestHandler
+    Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"🚀 服务器已启动：http://localhost:{PORT}")
-    print("📂 请确保 index.html 和其他文件位于当前目录")
-    print("⏳ 正在自动打开浏览器...")
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"🚀 服务器已启动：http://localhost:{PORT}")
+        print("📂 请确保 index.html 和其他文件位于当前目录")
+        print("⏳ 正在自动打开浏览器...")
+        
+        # 自动打开首页
+        webbrowser.open(f"http://localhost:{PORT}/index.html")
+        
+        print("🛑 按 Ctrl+C 停止服务器")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\n👋 服务器已停止")
+except Exception as e :
+    print(e)
     
-    # 自动打开首页
-    webbrowser.open(f"http://localhost:{PORT}/index.html")
-    
-    print("🛑 按 Ctrl+C 停止服务器")
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\n👋 服务器已停止")
+input("按任意键继续...")
